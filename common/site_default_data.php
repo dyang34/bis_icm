c1a4e8f03b8d2b6e2f7f794c2aeab7634b246f5e
dyang34 <dyang34@naver.com>
2023-01-26 16:41:34 +0900

    삼성화재 보험사 추가

--- common/site_default_data.php
+++ common/site_default_data.php
@@ -76,8 +76,8 @@ $arrCustomerType = [
 $arrInsuranceCompany = [
     "1"=>"CHUBB"
     ,"2"=>"메리츠화재"
-    ,"3"=>"MG손보"
-    ,"4"=>"DB손보"
+    ,"3"=>"MG손해보험"
+    ,"4"=>"DB손해보험"
     ,"5"=>"삼성화재"
 ];
 
